c17879411abcb11870c99a6a4d66e93c8b0ff87a
chomookun <chomookun@gmail.com>
2022-06-20 00:47:36 +0900

    .

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
     parameters {
         string(name: 'STAGE', defaultValue: params.STAGE ?:'dev', description: 'stage')
-        string(name: 'GRADLE_EXTRA_OPTION', defaultValue: params.GRADLE_EXTRA_OPTION ?:'--init-script init.gradle --stacktrace', description:'gradle extra option')
+        string(name: 'GRADLE_EXTRA_OPTION', defaultValue: params.GRADLE_EXTRA_OPTION ?:'--stacktrace', description:'gradle extra option')
         string(name: 'MAVEN_PUBLISH_URL', defaultValue: params.MAVEN_PUBLISH_URL ?:'http://___/repository/maven-snapshot/', description: 'Maven publish URL')
         credentials(credentialType: 'com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl',
                 name: 'MAVEN_CREDENTIALS',
